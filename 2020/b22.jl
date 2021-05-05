d02c97f8689f14094ba8499ba6db786bca83ec63
Stefan Knudsen <stefan.knudsen@samsung.com>
2021-05-04 20:42:17 -0400

    Attempt to catch inifinite loops

--- 2020/b22.jl
+++ 2020/b22.jl
@@ -1,19 +1,33 @@
+function reccombat(p1, p2, slow1, slow2, oddstep)
+    # run two games, one twice as fast? if the two games have the same cards, player 1 wins
+
+    if length(p1) == 0 || length(p2) == 0
+        return p1 > 0
+
+    elseif p1[1] <= length(p1) && p2[1] <= length(p2)
+        if oddstep
+        reccombat(p1[1:p1[1]], p2[1:p2[1]], )
+        else
+
+    elseif p1[1] > p2[1]
+        p1 = vcat(p1[2:end], p1[1], p2[1])
+        p2 = p2[2:end]
+        reccombat(p1, p2)
+
+    else
+        p2 = vcat(p2[2:end], p2[1], p1[1])
+        p1 = p1[2:end]
+        reccombat(p1, p2)
+    end
+end
+
 function main()
-    # input = split.(split(read("testin22.txt", String), "\n\n"), "\n")
-    input = split.(split(read("in22.txt", String), "\n\n"), "\n")
+    input = split.(split(read("testin22.txt", String), "\n\n"), "\n")
+    # input = split.(split(read("in22.txt", String), "\n\n"), "\n")
     p1 = parse.(Int, input[1][2:end])
     p2 = parse.(Int, input[2][2:end-1]) # only remove last element for in22.txt
 
-    while length(p1) > 0 && length(p2) > 0
-        if p1[1] > p2[1]
-            p1 = vcat(p1[2:end], p1[1], p2[1])
-            p2 = p2[2:end]
-        else
-            p2 = vcat(p2[2:end], p2[1], p1[1])
-            p1 = p1[2:end]
-        end
-    end
-
+    p1winner = reccombat(p1, p2)
 
     if length(p1) > length(p2)
         len = length(p1)
